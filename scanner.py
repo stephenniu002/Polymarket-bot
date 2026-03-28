6c06750295ea2a2c63589995f1e7724ecc3a550a
stephenniu002 <123157674+stephenniu002@users.noreply.github.com>
2026-03-28 04:34:56 -0400

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,8 +1,8 @@
 import requests
 
 # 把这里换成你刚刚配好的真实Token和纯数字ID
-TOKEN = "7788042130:AAFJZo9LVP1fmjZjfn8wOvnCXBzCJMIU2Wg"
-CHAT_ID = "7788042130"
+TOKEN = "8367780977:AAFocCdMasdtbfNs0ztxqNMGojitVDzK3Xs"
+CHAT_ID = "5739995837"
 
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 payload = {"chat_id": CHAT_ID, "text": "🔔 恭喜！你的机器人配置彻底成功了！"}
